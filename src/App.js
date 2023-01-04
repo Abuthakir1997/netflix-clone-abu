3edb6530b1e07b885fd2ffd7b76ab018dcb94ca3
Abu thakir <abudx.at@gmail.com>
2023-01-04 23:37:33 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { HomeScreen } from './Pages/HomeScreen/HomeScreen';
-import Banner from './components/Banner';
 import Login from './Pages/Login/Login';
-import Profile from './Pages/ProfileScreen/ProfileScreen';
 import ProfileScreen from './Pages/ProfileScreen/ProfileScreen';
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout, selectUser } from './features/userSlice';
@@ -30,59 +28,10 @@ function App() {
     });
 
     return unsubscribe;
-  }, []);
+  });
 
-  // const user = {
-  //   name: "jack",
-  //   email: "jack@gmail.com",
-  //   pass: "ldsf"
-  // }
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header> */}
       <Router>
         {!user ? <Login /> : (
           <Routes>
@@ -92,8 +41,6 @@ function App() {
         )}
 
       </Router>
-      {/* <HomeScreen /> */}
-      {/* <Banner></Banner> */}
     </div>
   );
 }
