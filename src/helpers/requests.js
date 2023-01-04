b9b9e2bf2556e96add597946f9bdfecab8e963da
Abu thakir <abudx.at@gmail.com>
2023-01-05 00:05:14 +0530

    useeffect dependency added

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -1,5 +1,5 @@
 const API_KEY = "540b3aec8ab463116ad55b1e5d49668e";
-export default {
+const keys = {
     fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`,
@@ -11,3 +11,5 @@ export default {
     fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`,
     fetchDocumentaries: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
+
+export default keys;
